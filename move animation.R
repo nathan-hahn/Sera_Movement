e729c0bb3c87adcf13a0c6b94517ac7139f12d71
nathan-hahn <nhahnwa@gmail.com>
2021-05-26 07:36:41 -0700

    Animate post-release movement for all cohorts.

---
+++ move animation.R
@@ -0,0 +1,77 @@
+##### Path Animation #####
+
+library(anipaths)
+library(ggmap)
+library(tidyverse)
+
+source("./Sera_Functions.R")
+
+# Load dataset and convert to latlong
+output <- read.csv('./movdata/Sera_datasets_10Mar21_HMMclassified_20210427.csv') 
+
+# remove missing relocs
+df <- output %>%
+  drop_na(x,y) %>%
+  droplevels()
+
+output.latlong <- AddLatLong(df)
+
+# get background map
+# https://console.cloud.google.com/google/maps-apis/overview
+ggmap::register_google(key = "AIzaSyAyZnyz0E5teo9SbaKvyvoxkV1sAz-ty10", write = TRUE)
+background <- ggmap::get_googlemap(center = c(37.800, 1.097),
+                                   zoom = 12,
+                                   maptype = "satellite")
+
+
+
+# create animation dataset - by cohort
+ele <- filter(output.latlong, cohort == 1) %>%
+  mutate(cohort = as.factor(cohort)) %>%
+  mutate(date = as.POSIXct(date, tz = "Africa/Nairobi")) 
+
+# animation function
+animate_paths(paths = ele,
+              delta.t = "day", # daily movement
+              coord = c("location.long", "location.lat"),
+              Time.name = "date",
+              covariate = 'cohort',
+              ID.name = "MovDataID",
+              background = background,
+              method = "mp4")
+
+
+
+##### Animate all cohorts together
+# create animation dataset - cohort
+ele <- filter(output.latlong, cohort %in% c(1,2,3)) %>%
+  mutate(date = as.POSIXct(date, tz = "Africa/Nairobi")) %>%
+  filter(as.Date(date) >= as.Date("2019-05-02")) %>%
+  mutate(cohort = as.factor(cohort))
+
+
+color.pal <- c('#a50f14', '#de2d26', '#fb6a4a', #red
+               '#006d2c', '#31a354', '#74c476', #green  
+               '#08519c', '#3182bd') #blue
+
+# animation function
+animate_paths(paths = ele,
+              delta.t = "day", # daily movement
+              coord = c("location.long", "location.lat"),
+              Time.name = "date",
+              ID.name = "MovDataID",
+              pt.cex = 2,
+              pt.colors = color.pal,
+              background = background,
+              tail.wd = .5,
+              tail.length = 1,
+              method = "mp4")
+
+write.csv(ele, './movdata/animation_data.csv')
+
+t <- ele %>% filter(cohort == 3)
+write.csv(t, './movdata/animation_data_cohort3.csv')
+
+
+
+
